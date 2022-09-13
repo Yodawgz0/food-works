0e5fc51e1800d682c127df46433e07739d6a65c3
skandhanM <skandhanmnss208@gmail.com>
2022-09-13 23:58:01 +0530

    header part updated

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -3,7 +3,20 @@ import React from "react";
 export default function HeaderMain() {
   return (
     <div className="HeaderContainer">
-      <h2>This is the header</h2>
+      <h2>___</h2>
+      <h1>DEEPLIGHT</h1>
+      <h5>RESTAURANT</h5>
+    <nav id="navbar">
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>Menu</a></li>
+        <li><a>About</a></li>
+        <li><a>Gallery</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </nav>
     </div>
+
   );
 }
