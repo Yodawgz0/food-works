66ee2aaa6f6b34874f4d21e6941da395af7ca982
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-09-13 20:06:41 +0530

    adding Home Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import HeaderMain from "./components/HeaderMain";
 import FooterMain from "./components/FooterMain";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <HeaderMain />
+      <Home />
       <FooterMain />
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="HomeContainer">
+      <h2>This is the Home</h2>
+    </div>
+  );
+}
