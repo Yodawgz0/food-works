204c9e90358bfa03cd5ec1e0739280187171e9b2
ZSG_Hikari <sangoten24@gmail.com>
2022-09-15 15:46:25 +0530

    Changed image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,15 @@ import React from "react";
 export default function Home() {
   return (
     <div className="HomeContainer">
-      {<img src={require('./pic1.webp')} /> }
+      {<img src={require('./Mainpic.jpg')} /> }
+      
       <div className="InviteContainer">
         <h2>delightful experiences</h2>
         <h1>Welcome to "Deeplight Restaurant"</h1>
         <button>Reservation</button>
       </div>
     </div>
+    
 
 
   );
