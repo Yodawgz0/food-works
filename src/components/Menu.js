54db46adae49fedbd4558e56ec686601e6f8759a
skandhanM <skandhanmnss208@gmail.com>
2022-09-17 12:49:08 +0530

    completed the menu section

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Chefrec from "../assets/chefrec_img.png"
+import maincourse from "../assets/maincourse_img.png"
 
 export default function Menu(){
     return(
@@ -34,6 +35,36 @@ export default function Menu(){
                 <img alt="SampleImage" src={Chefrec} />
                 </div>
             </div>
+
+            <div className="menulist">
+            <div className="menulist__chefimage">
+                <img alt="SampleImage" src={maincourse} />
+                </div>
+                <div className="menulist__chefrecommend">
+                <h1>Chef Recommend</h1>
+                <section className="dishname">
+                    <h2 className="dishname__mainname">Lamb Salad With Fregola<span> ---------$32.5</span></h2>
+                    <h3 className="dishname__desc">Lamb/fregola</h3>
+                </section>
+                <section className="dishname">
+                    <h2 className="dishname__mainname">Lamb Salad With Fregola<span> ---------$32.5</span></h2>
+                    <h3 className="dishname__desc">Lamb/fregola</h3>
+                </section>
+                <section className="dishname">
+                    <h2 className="dishname__mainname">Lamb Salad With Fregola<span> ---------$32.5</span></h2>
+                    <h3 className="dishname__desc">Lamb/fregola</h3>
+                </section>
+                <section className="dishname">
+                    <h2 className="dishname__mainname">Lamb Salad With Fregola<span> ---------$32.5</span></h2>
+                    <h3 className="dishname__desc">Lamb/fregola</h3>
+                </section>
+                </div>
+                
+            </div>
+            <div className="endredline">
+                    <div className="endredline__redline"></div>
+                </div>
+        
         
         
         
