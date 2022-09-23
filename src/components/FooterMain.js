0139f0dbd7911d15f4b34f13110cd537925b93b7
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-09-23 19:55:12 +0530

    refactor

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -1,28 +1,39 @@
 import React from "react";
-import { faFacebook , faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FooterPic from "../assets/footer.png";
 
 export default function FooterMain() {
   return (
     <div className="footerContainer">
-      <img alt="FooterPic" src={FooterPic}/>
+      <img alt="FooterPic" src={FooterPic} />
 
       <div className="footContainer">
-      
-      <h1 className="footContainer__mainheading">DEEPLIGHT</h1>
-      <h5 className="footContainer__subheading">RESTAURANT</h5>
+        <h1 className="footContainer__mainheading">DEEPLIGHT</h1>
+        <h5 className="footContainer__subheading">RESTAURANT</h5>
         <h5 className="footContainer__contact">You can reach us at</h5>
         <div className="footContainer__blackline"></div>
         <h1 className="footContainer__number">+1 123 456 7890</h1>
-    
-      <h5 className="footContainer__address">Address</h5>
-      <h6 className="footContainer__mainaddress">13 Fifth Avenue, New York</h6>
-    
-      <a className="footContainer__icons" href="https://facebook.com"><FontAwesomeIcon icon={faFacebook} size='2x' color="black"/></a>
-      <a className="footContainer__icons" href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} size='2x' color="black"/></a>
-      <a className="footContainer__icons" href="https://twitter.com"><FontAwesomeIcon icon={faTwitter} size='2x' color="black"/></a>
-    </div>
+
+        <h5 className="footContainer__address">Address</h5>
+        <h6 className="footContainer__mainaddress">
+          13 Fifth Avenue, New York
+        </h6>
+
+        <a className="footContainer__icons" href="https://facebook.com">
+          <FontAwesomeIcon icon={faFacebook} size="2x" color="black" />
+        </a>
+        <a className="footContainer__icons" href="https://instagram.com">
+          <FontAwesomeIcon icon={faInstagram} size="2x" color="black" />
+        </a>
+        <a className="footContainer__icons" href="https://twitter.com">
+          <FontAwesomeIcon icon={faTwitter} size="2x" color="black" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,30 +1,35 @@
 import React from "react";
-import BgPic from "../assets/test.png"
-import ReservePic from "../assets/reservee.png"
+import BgPic from "../assets/test.png";
+import ReservePic from "../assets/reservee.png";
 
 export default function Testimonials() {
-    return(
-        <div className="testcontainer">
-            <img alt="BgPic" src={BgPic} />
-            <img alt="ReservePic" src={ReservePic} className="centerpic"/>
-        <div className="testimonialcontainer">
-           
-           
-            <h2 className="testimonialcontainer__mainheading">Our customers Fall in Love With Us</h2>
-            <div className="testimonialcontainer__blackline"></div>
-            <p className="testimonialcontainer__details">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium laudantium, 
-            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-            dicta sunt explicabo."</p>
-            <div className="testimonialcontainer__redline"></div>
-        </div>
-            <div className="ReservationContainer">
-                <h3 className="ReservationContainer__smallheading">join us</h3>
-                <h2 className="ReservationContainer__heading">Book a table Now, Come and See Us</h2>
-                <button className="ReservationContainer__reservebtn">Reservation</button>
-                <div className="ReservationContainer__bottomredline"></div>
-            </div>
-        </div>
-    
-    );
+  return (
+    <div className="testcontainer">
+      <img alt="BgPic" src={BgPic} />
+      <img alt="ReservePic" src={ReservePic} className="centerpic" />
+      <div className="testimonialcontainer">
+        <h2 className="testimonialcontainer__mainheading">
+          Our customers Fall in Love With Us
+        </h2>
+        <div className="testimonialcontainer__blackline"></div>
+        <p className="testimonialcontainer__details">
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium laudantium, totam rem aperiam, eaque ipsa quae ab illo
+          inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo."
+        </p>
+        <div className="testimonialcontainer__redline"></div>
+      </div>
+      <div className="ReservationContainer">
+        <h3 className="ReservationContainer__smallheading">join us</h3>
+        <h2 className="ReservationContainer__heading">
+          Book a table Now, Come and See Us
+        </h2>
+        <button className="ReservationContainer__reservebtn">
+          Reservation
+        </button>
+        <div className="ReservationContainer__bottomredline"></div>
+      </div>
+    </div>
+  );
 }
