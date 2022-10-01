dced9591d79a5e1a5af7c5a688998ad668e88dfe
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-10-01 15:41:36 +0530

    comments

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-  toggleButton.addEventListener('click',() => {
-    navbarLinks.classList.toggle('active')
-  })
-
 export default function HeaderMain() {
-  
-  
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+  const handlePhoneMenu = () => {};
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
 
   const navDetails = [
     "Home",
@@ -23,46 +19,57 @@ export default function HeaderMain() {
   ];
   return (
     <div className="Parent">
-    <nav className="navbar1">
-      <div className="Options">Options</div>
-      <a href="#" className="toggle-button">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </a>
-      <div className="navbar-links">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Gallery</a></li>
-          <li><a href="#">Testimonials</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-
-      </div>
-    </nav>
-    
-    <div className="HeaderContainer">
-      <h2>___</h2>
-      <h1 className="HeaderContainer__mainHeading">DEEPLIGHT</h1>
-      <h5 className="HeaderContainer__subHeading">RESTAURANT</h5>
-      <nav className="navbar">
-        <ul className="navbar__ulattributes">
-          {navDetails.map((element, index) => {
-            return (
-              <li className="navbar__liattributes">
-                <a className="navbar__navlinks" key={index} href="{#}">
-                  {element}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <nav className="navbar1">
+        <div className="Options">Options</div>
+        <a href="#" className="toggle-button">
+          {/* Make this Icon */}
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+        <div className="navbar-links">
+          {/* Map please here  */}
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Gallery</a>
+            </li>
+            <li>
+              <a href="#">Testimonials</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </nav>
-    </div>
+
+      <div className="HeaderContainer">
+        <h2>___</h2>
+        <h1 className="HeaderContainer__mainHeading">DEEPLIGHT</h1>
+        <h5 className="HeaderContainer__subHeading">RESTAURANT</h5>
+        <nav className="navbar">
+          <ul className="navbar__ulattributes">
+            {navDetails.map((element, index) => {
+              return (
+                <li className="navbar__liattributes">
+                  <a className="navbar__navlinks" key={index} href="{#}">
+                    {element}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
-
-
 }
