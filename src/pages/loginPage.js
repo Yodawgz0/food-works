7090bdd56c3fcdec7527245f46745b4a491635f3
skandhanM <skandhanmnss208@gmail.com>
2022-10-01 11:01:56 +0530

    signup login page border and container done

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function Login() {
   return (
     <>
-      <div>loginPage</div>
+      <div className="maincontainer">
+        <div className="maincontainer__bord1">
+          <div className="maincontainer__bord2">
+            <div className="maincontainer__bord3">
+              <div className="maincontainer__bord4">
+                <div className="maincontainer__formcontainer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
