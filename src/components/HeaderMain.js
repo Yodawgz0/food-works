cfcb0fd461b3694ba86fd08c16b4b908aef941dc
ZSG_Hikari <sangoten24@gmail.com>
2022-10-08 14:43:41 +0530

    Mapping

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+
+
 export default function HeaderMain() {
-  //const toggleButton = document.getElementsByClassName("toggle-button")[0];
-  //const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-  //const handlePhoneMenu = () => {};
-  //toggleButton.addEventListener("click", () => {
-    //navbarLinks.classList.toggle("active");
-  //});
-
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  
+  const handlePhoneMenu = () => {};
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
+  
   const navDetails = [
     "Home",
     "Menu",
@@ -24,29 +26,23 @@ export default function HeaderMain() {
         <div className="Options">Options</div>
         <a href="#" className="toggle-button">
           {/* Make this Icon */}
-          <FontAwesomeIcon icon="fa-solid fa-bars" />
+          {/*<FontAwesomeIcon icon="fa-solid fa-bars" />*/}
+           <span className="bar"></span>
+           <span className="bar"></span>
+          <span className="bar"></span>
         </a>
         <div className="navbar-links">
           {/* Map please here  */}
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Menu</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Gallery</a>
-            </li>
-            <li>
-              <a href="#">Testimonials</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
+          <ul className="navbar__ulattributes">
+            {navDetails.map((element, index) => {
+              return (
+                <li className="navbar__liattributes">
+                  <a className="navbar__navlinks" key={index} href="{#}">
+                    {element}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
@@ -72,3 +68,4 @@ export default function HeaderMain() {
     </div>
   );
 }
+
