8c520e88c6c45765b098d8e2fb0d554499bd5160
Esha <eshamr294@gmail.com>
2022-10-11 20:08:59 +0530

    minor changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -60,25 +60,23 @@ export default function Menu() {
     <div className="menucontainer">
       <div className="menucontainer__redline"></div>
       <div className="menucontainer__suggestionheading">
-        <h1>eat good food</h1>
+        <h1>Eat good food</h1>
       </div>
       <div className="menucontainer__mainheading">
-        <h1>Have A Quick Look At Our Menus</h1>
+        <h1>Have a quick look at our menu</h1>
       </div>
       <div className="menucontainer__blackline"></div>
       <div className="menucontainer__menudesc">
         <p>
           Le Deeplight est simplement du faux texte employé dans la composition
           et la mise en page avant impression. Le Lorem Ipsum est le faux texte
-          standard de l'imprimerie depuis les années 1500, quand un imprimeur
-          anonyme assembla ensemble des morceaux de texte pour réaliser un livre
-          spécimen de polices de texte.
+          standard.
         </p>
       </div>
       {/* start of the menu part */}
       <div className="menulist">
         <div className="menulist__chefrecommend">
-          <h1>Chef Recommend</h1>
+          <h1>Chef Recommends</h1>
           {fooditem.map((element, index) => {
             return index < 4 ? (
               <section className="dishname">
@@ -103,7 +101,7 @@ export default function Menu() {
           <img alt="SampleImage" src={maincourse} />
         </div>
         <div className="menulist__chefrecommend">
-          <h1>We Recommend</h1>
+          <h1>Our recommendations</h1>
           {fooditem.map((element, index) => {
             return index > 3 ? (
               <section key={index} className="dishname">
